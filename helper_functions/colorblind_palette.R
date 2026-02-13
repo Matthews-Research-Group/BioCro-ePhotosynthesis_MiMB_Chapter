c792650207034cfef7801e907aaa91b8e3e93524
uphone87 <yufenghe87@gmail.com>
2026-02-13 14:04:32 -0600

    add a colorblind friendly pallete

---
+++ helper_functions/colorblind_palette.R
@@ -0,0 +1,13 @@
+cb8<-function(){
+ tmp <- c(
+  "#000000",  # Black
+  "#E69F00",  # Orange
+  "#56B4E9",  # Sky blue
+  "#009E73",  # Bluish green
+  "#F0E442",  # Yellow
+  "#0072B2",  # Blue
+  "#D55E00",  # Vermillion
+  "#CC79A7"   # Reddish purple
+ )
+ return(tmp)
+}
